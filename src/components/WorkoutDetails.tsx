507b408dea3f5fd8cad49e03b540c04372a6ad5e
jhonnierandrey <jhonnierandrey@outlook.com>
2023-05-15 21:35:44 -0300

    12 - deleting data

--- src/components/WorkoutDetails.tsx
+++ src/components/WorkoutDetails.tsx
@@ -1,16 +1,28 @@
 import { Workout } from '../types/types'
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
 
 type WorkoutDetailsProps = {
     workout: Workout
 }
 
 const WorkoutDetails = ({ workout }: WorkoutDetailsProps) => {
+    const { dispatch } = useWorkoutsContext();
+    const handleClick = async () => {
+        const response = await fetch(`/api/workouts/${workout._id}`, {
+            method: 'DELETE',
+        })
+        const json = await response.json()
+        if (response.ok) {
+            dispatch({ type: 'DELETE_WORKOUT', payload: json })
+        }
+    }
     return (
         <div className='workout-details'>
             <h4>{workout.title}</h4>
             <p><strong>Load (kg):</strong>{workout.load}</p>
             <p><strong>Reps:</strong>{workout.reps}</p>
             <p>{workout.createdAt}</p>
+            <span onClick={handleClick}>Delete</span>
         </div>
     )
 }
--- src/context/WorkoutContext.tsx
+++ src/context/WorkoutContext.tsx
@@ -22,6 +22,10 @@ export const workoutReducer = (state: WorkoutContextStateType, action: any) => {
             return {
                 workouts: [action.payload, ...state.workouts]
             }
+        case 'DELETE_WORKOUT':
+            return {
+                workouts: state.workouts.filter((workout) => workout._id !== action.payload._id)
+            }
         default:
             return state
     }
