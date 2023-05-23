0699f725e1be21a0ad5a5fed8e5f3b0636829026
jhonnierandrey <jhonnierandrey@outlook.com>
2023-05-23 20:14:05 -0300

    auth-12 - making a uselogin hook

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,14 +1,17 @@
 import React, { useState } from 'react'
+import { useLogin } from '../hooks/useLogin'
 
 const Login = () => {
 
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
+    const { login, error, isLoading } = useLogin()
+
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
 
-        console.log(email, password);
+        await login(email, password);
     }
 
     return (
@@ -18,7 +21,8 @@ const Login = () => {
             <input type="email" onChange={(e) => setEmail(e.target.value)} value={email} />
             <label>Password</label>
             <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
-            <button type="submit">Login</button>
+            <button type="submit" disabled={isLoading}>Login</button>
+            {error && <div className='error'>{error}</div>}
         </form>
     )
 }
