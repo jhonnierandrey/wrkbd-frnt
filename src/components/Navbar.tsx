6178f1d17b1f02daebbf60c56090220862b32c3f
jhonnierandrey <jhonnierandrey@outlook.com>
2023-05-23 20:31:58 -0300

    auth-13 - getting the initial aut status

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom"
 import { useLogout } from "../hooks/useLogout"
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Navbar = () => {
     const { logout } = useLogout();
+    const { state } = useAuthContext();
 
     const handleClick = () => {
         logout()
@@ -14,13 +16,18 @@ const Navbar = () => {
                     Workout Buddy
                 </Link>
                 <nav>
-                    <div>
-                        <button onClick={handleClick}>Logout</button>
-                    </div>
-                    <div>
-                        <Link to="/login">Login</Link>
-                        <Link to="/signup">Sign up</Link>
-                    </div>
+                    {
+                        state.user ?
+                            <div>
+                                <span>{state?.user?.email}</span>
+                                <button onClick={handleClick}>Logout</button>
+                            </div>
+                            :
+                            <div>
+                                <Link to="/login">Login</Link>
+                                <Link to="/signup">Sign up</Link>
+                            </div>
+                    }
                 </nav>
             </div>
         </header>
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,7 +1,10 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 
 type AuthContextStateType = {
-    user: ""
+    user: {
+        email: string,
+        token: string,
+    }
 }
 
 type AuthContextType = {
@@ -30,6 +33,13 @@ export const AuthContextProvider = ({ children }: React.PropsWithChildren) => {
     const [state, dispatch] = useReducer(authReducer, {
         user: null
     })
+
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem('user') || "");
+        if (user) {
+            dispatch({ type: 'LOGIN', payload: user });
+        }
+    }, [])
 
     console.log(`AuthContext state: ${state}`)
 
