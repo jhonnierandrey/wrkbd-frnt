05274e12fede8bb54d27dc00570338627ea1e2e9
jhonnierandrey <jhonnierandrey@outlook.com>
2023-05-23 19:54:18 -0300

    auth-10 - making useSignup hook

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -15,9 +15,9 @@ const Login = () => {
         <form className="login" onSubmit={handleSubmit}>
             <h3>Login</h3>
             <label>Email</label>
-            <input type="email" name="" id="" onChange={(e) => setEmail(e.target.value)} value={email} />
+            <input type="email" onChange={(e) => setEmail(e.target.value)} value={email} />
             <label>Password</label>
-            <input type="password" name="" id="" onChange={(e) => setPassword(e.target.value)} value={password} />
+            <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
             <button type="submit">Login</button>
         </form>
     )
--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,24 +1,28 @@
 import React, { useState } from 'react'
+import { useSignup } from '../hooks/useSignup'
 
 const Signup = () => {
 
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
+    const { signup, error, isLoading } = useSignup();
+
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
 
-        console.log(email, password);
+        await signup(email, password);
     }
 
     return (
         <form className="signup" onSubmit={handleSubmit}>
             <h3>Sign up</h3>
             <label>Email</label>
-            <input type="email" name="" id="" onChange={(e) => setEmail(e.target.value)} value={email} />
+            <input type="email" onChange={(e) => setEmail(e.target.value)} value={email} />
             <label>Password</label>
-            <input type="password" name="" id="" onChange={(e) => setPassword(e.target.value)} value={password} />
-            <button type="submit">Sign up</button>
+            <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
+            <button type="submit" disabled={isLoading}>Sign up</button>
+            {error && <div className='error'>{error}</div>}
         </form>
     )
 }
