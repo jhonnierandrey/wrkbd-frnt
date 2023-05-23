7c60892d343d7998f0d06c876fb0f67faa5f7afc
jhonnierandrey <jhonnierandrey@outlook.com>
2023-05-23 19:17:47 -0300

    auth-09 - login and signup forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
           </Routes>
         </div>
       </Router>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,6 +7,12 @@ const Navbar = () => {
                 <Link to="/">
                     Workout Buddy
                 </Link>
+                <nav>
+                    <div>
+                        <Link to="/login">Login</Link>
+                        <Link to="/signup">Sign up</Link>
+                    </div>
+                </nav>
             </div>
         </header>
     )
