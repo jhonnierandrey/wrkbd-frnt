39461cbcc9c69c4d2898d79663e860dcd84f2bcf
jhonnierandrey <jhonnierandrey@outlook.com>
2023-05-27 22:35:17 -0300

    auth-16 - protecting react routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Login from './components/Login';
 import Signup from './components/Signup';
 
+import { useAuthContext } from './hooks/useAuthContext';
+
 function App() {
+  const { state } = useAuthContext();
+
+  const user = state.user;
+
   return (
     <div className="App">
       <Router>
         <Navbar />
         <div className="pages">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
+            <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
+            <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
+            <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" />} />
           </Routes>
         </div>
       </Router>
